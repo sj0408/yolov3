5371cfcab6e0a3c0f73415d670fc954c3466f403
sj0408 <38971715+sj0408@users.noreply.github.com>
2020-11-05 10:32:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -185,7 +185,7 @@ def test(cfg,
             final_outputs.append(cat_output)
         
         # NMS for original image and stiched image
-        output = non_max_suppression_ver_2(final_outputs, conf_thres=0.05, iou_thres=0.4)
+        output = non_max_suppression_ver_2(final_outputs, conf_thres=conf_thres, iou_thres=iou_thres)
 
         # Statistics per image
         for si, pred in enumerate(output):
